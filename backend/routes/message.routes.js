60d6ecea2533ecfbe8f261284acd168eb326a079
daniyalansarii <162056842+daniyalansarii@users.noreply.github.com>
2025-08-19 04:10:28 +0500

    Update message.routes.js

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,13 +1,13 @@
 // routes/message.routes.js
 import express from "express";
 import { sendMessage, getMessages } from "../controllers/message.controller.js";
-import protect from "../middlewares/protect.js";
+import protect from "../middlewares/isAuth.js";
 // if you use multer/cloudinary: import upload from "../middlewares/upload.js";
 
 const router = express.Router();
 
 // NOTE: param name is :receiver (used by controller)
-router.post("/send/:receiver", protect, /* upload.single("image"), */ sendMessage);
-router.get("/:receiver", protect, getMessages);
+router.post("/send/:receiver", isAuth, /* upload.single("image"), */ sendMessage);
+router.get("/:receiver", isAuth, getMessages);
 
 export default router;
