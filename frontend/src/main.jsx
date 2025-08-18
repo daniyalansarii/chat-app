e0d9a2716b46e8e125fc0db2331b5018634813b4
daniyalansarii <162056842+daniyalansarii@users.noreply.github.com>
2025-08-18 23:31:46 +0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-export const serverUrl = "http://localhost:8000";
+export const serverUrl = "https://realtime-chat-app-backend-lvdc.onrender.com";
 import {Provider} from 'react-redux'
 import { store } from "./redux/store.js";
 
