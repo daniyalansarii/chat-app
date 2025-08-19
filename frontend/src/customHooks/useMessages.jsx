a3e4750e914973728e3a37e3010711f778fe37ef
daniyalansarii <162056842+daniyalansarii@users.noreply.github.com>
2025-08-19 06:26:23 +0500

    Update and rename getMessages.jsx to useMessages.jsx

--- frontend/src/customHooks/useMessages.jsx
+++ frontend/src/customHooks/useMessages.jsx
@@ -1,20 +1,21 @@
+// customHooks/useMessages.js
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setMessages } from "../redux/messageslice.js";
 import axios from "axios";
 import { serverUrl } from "../main.jsx";
 
-const getMessage = () => {
+const useMessages = () => {
   const dispatch = useDispatch();
-  const { selectedUser, userData } = useSelector((state) => state.user);
+  const { selectedUser } = useSelector((state) => state.user);
 
   useEffect(() => {
-    if (!selectedUser) return; // Prevent error if no user is selected
+    if (!selectedUser) return;
 
     const fetchMessages = async () => {
       try {
         const result = await axios.get(
-          `${serverUrl}/api/message/get/${selectedUser._id}`,
+          `${serverUrl}/api/message/${selectedUser._id}`,
           { withCredentials: true }
         );
         dispatch(setMessages(result.data));
@@ -24,7 +25,7 @@ const getMessage = () => {
     };
 
     fetchMessages();
-  }, [selectedUser, userData, dispatch]);
+  }, [selectedUser, dispatch]);
 };
 
-export default getMessage;
+export default useMessages;
