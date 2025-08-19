98d8c3241d6b718ad041c2f65cc353ffdb5f950d
daniyalansarii <162056842+daniyalansarii@users.noreply.github.com>
2025-08-19 05:45:12 +0500

    Update getMessages.jsx

--- frontend/src/customHooks/getMessages.jsx
+++ frontend/src/customHooks/getMessages.jsx
@@ -1,27 +1,30 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setUserData } from "../redux/userSlice.js";
+import { setMessages } from "../redux/messageslice.js";
 import axios from "axios";
 import { serverUrl } from "../main.jsx";
-import { setMessages } from "../redux/messageslice.js";
 
 const getMessage = () => {
   const dispatch = useDispatch();
-  let { userData,selectedUser } = useSelector((state) => state.user);
+  const { selectedUser, userData } = useSelector((state) => state.user);
 
   useEffect(() => {
-    const fetchMessage = async () => {
+    if (!selectedUser) return; // Prevent error if no user is selected
+
+    const fetchMessages = async () => {
       try {
-        const result = await axios.get(`${serverUrl}/api/message/get/${selectedUser._id}`, {
-          withCredentials: true,
-        });
+        const result = await axios.get(
+          `${serverUrl}/api/message/get/${selectedUser._id}`,
+          { withCredentials: true }
+        );
         dispatch(setMessages(result.data));
       } catch (error) {
-        console.error("Error fetching user:", error);
+        console.error("Error fetching messages:", error);
       }
     };
 
-    fetchMessage();
-  }, [selectedUser,userData]);
+    fetchMessages();
+  }, [selectedUser, userData, dispatch]);
 };
+
 export default getMessage;
