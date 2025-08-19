c850db78664908c53ed30beb161a8ff88c0fa0d5
daniyalansarii <162056842+daniyalansarii@users.noreply.github.com>
2025-08-19 06:48:20 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,27 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import connectDb from './config/db.js'
-import authRouter from './routes/auth.routes.js'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import userRouter from './routes/user.routes.js'
-import messageRouter from './routes/message.routes.js'
-import { app, server } from './socket/socket.js'
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
+import messageRouter from "./routes/message.routes.js";
+import { app, server } from "./socket/socket.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-dotenv.config()
-
-const port = process.env.PORT || 5000
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: "https://chat-appp-ktmw.onrender.com",
-    credentials: true
-}))
-app.use(express.json())
-app.use(cookieParser())
+  origin: "https://chat-appp-ktmw.onrender.com",
+  credentials: true,
+}));
+app.use(cookieParser());
+app.use(express.json());
 
-app.use("/api/auth", authRouter)
-app.use("/api/user", userRouter)
-app.use("/api/message", messageRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/message", messageRouter);
 
-server.listen(port, () => {
-    connectDb()
-    console.log(`✅ Server running on port ${port}`)
-})
+server.listen(PORT, () => {
+  connectDb();
+  console.log(`Server running on port ${PORT}`);
+});
