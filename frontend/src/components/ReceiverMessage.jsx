c7bc28ae71a05a9950766e91ef08878f5dd570eb
daniyalansarii <162056842+daniyalansarii@users.noreply.github.com>
2025-08-19 05:50:01 +0500

    Update ReceiverMessage.jsx

--- frontend/src/components/ReceiverMessage.jsx
+++ frontend/src/components/ReceiverMessage.jsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 
 function ReceiverMessage({ image, message }) {
   const scroll = useRef();
-  let {selectedUser} = useSelector(state=>state.user)
+  const { selectedUser } = useSelector((state) => state.user);
 
   useEffect(() => {
     if (scroll.current) {
@@ -19,30 +19,32 @@ function ReceiverMessage({ image, message }) {
   };
 
   return (
-     <div className="flex items-start gap-2">
-      <div className="w-[40px] shadow-lg shadow-gray-500 h-[40px] rounded-full overflow-hidden flex justify-center items-center cursor-pointer bg-white ">
-            <img
-              src={selectedUser.image || dp}
-              className="h-[100%]"
-              alt=""
-            />
-          </div>
-          <div
-            ref={scroll}
-            className="w-fit max-w-[500px] px-5 py-2 bg-[rgb(23,151,194)] text-white text-[19px] rounded-2xl rounded-tl-none relative left-0  gap-5 flex flex-col"
-          >
-            {image && (
-              <img
-                className="w-36 rounded-lg"
-                src={image}
-                alt=""
-                onLoad={handleImageScroll}
-              />
-            )}
-            {message && <span>{message}</span>}
-          </div>
-          
-        </div>
+    <div className="flex items-start gap-2 justify-start">
+      {/* Avatar */}
+      <div className="w-[40px] h-[40px] rounded-full overflow-hidden flex justify-center items-center shadow-lg shadow-gray-500 bg-white cursor-pointer">
+        <img
+          src={selectedUser?.image || dp}
+          className="h-[100%]"
+          alt={selectedUser?.name || "User"}
+        />
+      </div>
+
+      {/* Message Bubble */}
+      <div
+        ref={scroll}
+        className="w-fit max-w-[500px] px-5 py-2 bg-gray-200 text-black text-[19px] rounded-2xl rounded-tl-none flex flex-col gap-2"
+      >
+        {image && (
+          <img
+            className="w-36 rounded-lg"
+            src={image}
+            alt="attachment"
+            onLoad={handleImageScroll}
+          />
+        )}
+        {message && <span>{message}</span>}
+      </div>
+    </div>
   );
 }
 
